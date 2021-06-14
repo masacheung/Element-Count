7ecc2fcdccb297e27f96e8e6331ed962d22d585f
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-06-14 19:31:01 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,11 @@
+def element_count(arr)
+	count = Hash.new(0)
+  	
+  	arr.each do |str|
+      count[str] += 1
+    end
+  return count
+end
+
+puts element_count(["a", "b", "a", "a", "b"]) #=> {"a"=>3, "b"=>2}
+puts element_count(["red", "red", "blue", "green"]) #=> {"red"=>2, "blue"=>1, "green"=>1}
